1d8b700d47c53becabaf5e370b578d5ae2786d16
adammarchel123 <adammarchel123@gmail.com>
2021-04-15 16:37:36 +0700

    part 18

--- app/src/main/java/com/example/kitzhen/ui/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/kitzhen/ui/fragment/recipes/RecipesFragment.kt
@@ -1,10 +1,11 @@
-package com.example.kitzhen
+package com.example.kitzhen.ui.fragment.recipes
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kitzhen.R
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
---
+++ app/src/main/java/com/example/kitzhen/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.kitzhen.util
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "9f1689aa106a46939664a95f784cd43f"
+    }
+}
