6dc0c0280b6cbf0076042a82786d9e4fa060965b
adammarchel123 <adammarchel123@gmail.com>
2021-05-01 16:41:38 +0700

    part 23

--- app/src/main/java/com/example/kitzhen/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/kitzhen/data/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.kitzhen.data
 
-class RemoteDataSource() {
+import com.example.kitzhen.data.network.FoodRecipesApi
+import com.example.kitzhen.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
