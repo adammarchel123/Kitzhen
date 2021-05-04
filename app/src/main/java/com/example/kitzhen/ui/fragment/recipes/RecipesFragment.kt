0310e971c4cf7b384bc68e655ba21ae2b5522047
adammarchel123 <adammarchel123@gmail.com>
2021-05-05 04:21:21 +0700

    part 36

--- app/src/main/java/com/example/kitzhen/ui/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/kitzhen/ui/fragment/recipes/RecipesFragment.kt
@@ -5,22 +5,73 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kitzhen.R
+import com.example.kitzhen.adapters.RecipesAdapter
+import com.example.kitzhen.util.Constants.Companion.API_KEY
+import com.example.kitzhen.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.kitzhen.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.kitzhen.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
+import com.example.kitzhen.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
+import com.example.kitzhen.util.Constants.Companion.QUERY_API_KEY
+import com.example.kitzhen.util.Constants.Companion.QUERY_DIET
+import com.example.kitzhen.util.Constants.Companion.QUERY_FILL_INGREDIENTS
+import com.example.kitzhen.util.Constants.Companion.QUERY_NUMBER
+import com.example.kitzhen.util.Constants.Companion.QUERY_TYPE
+import com.example.kitzhen.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 
 class RecipesFragment : Fragment() {
+
+    private lateinit var mainViewModel: MainViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
+    private lateinit var mView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
 
-        view.recyclerview.showShimmer()
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
-        return view
+        setupRecyclerView()
+
+        return mView
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
+        queries[QUERY_API_KEY] = API_KEY
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
+        queries[QUERY_FILL_INGREDIENTS] = "true"
+
+        return queries
+    }
+
+    private fun setupRecyclerView() {
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        mView.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        mView.recyclerview.hideShimmer()
     }
 
 }
--- app/src/main/java/com/example/kitzhen/util/Constants.kt
+++ app/src/main/java/com/example/kitzhen/util/Constants.kt
@@ -6,5 +6,16 @@ class Constants {
 
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "9f1689aa106a46939664a95f784cd43f"
+
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+        const val DEFAULT_RECIPES_NUMBER = "50"
+        const val DEFAULT_MEAL_TYPE = "main course"
+        const val DEFAULT_DIET_TYPE = "gluten free"
     }
 }
