39594bf61113fd2db58c5cc659d17da88559c35c
adammarchel123 <adammarchel123@gmail.com>
2021-05-04 18:51:20 +0700

    part 27

---
+++ app/src/main/java/com/example/kitzhen/adapters/RecipesAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.kitzhen.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesAdapter.MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecipesAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
