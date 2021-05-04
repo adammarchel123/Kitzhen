148b2b1e6a2f7d65013584e28d6dd2ec7b8bd1f8
adammarchel123 <adammarchel123@gmail.com>
2021-05-04 19:07:03 +0700

    part 29

--- app/src/main/java/com/example/kitzhen/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/example/kitzhen/adapters/RecipesAdapter.kt
@@ -1,21 +1,49 @@
 package com.example.kitzhen.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kitzhen.databinding.RecipesRowLayoutBinding
+import com.example.kitzhen.models.FoodRecipe
+import com.example.kitzhen.models.Result
+
 
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
+    private var recipes = emptyList<Result>()
 
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesAdapter.MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: RecipesAdapter.MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return recipes.size
+    }
+
+    fun setData(newData: FoodRecipe) {
+        recipes = newData.results
+        notifyDataSetChanged()
     }
 }
