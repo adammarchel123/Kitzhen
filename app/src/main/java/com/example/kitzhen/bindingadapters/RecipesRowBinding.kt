feee40d4cb8ee7f5b84e88e51b3c12a8f26facee
adammarchel123 <adammarchel123@gmail.com>
2021-05-04 21:31:50 +0700

    part 34

--- app/src/main/java/com/example/kitzhen/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/kitzhen/bindingadapters/RecipesRowBinding.kt
@@ -1,7 +1,12 @@
 package com.example.kitzhen.bindingadapters
 
+import android.graphics.Color.green
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.example.kitzhen.R
 
 class RecipesRowBinding {
 
@@ -18,6 +23,32 @@ class RecipesRowBinding {
         fun setNumberOfMinutes(textView: TextView, minutes: Int) {
             textView.text = minutes.toString()
         }
+
+        @BindingAdapter("applyVeganColor")
+        @JvmStatic
+        fun applyVeganColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
+                        view.setTextColor(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                    is ImageView -> {
+                        view.setColorFilter(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                }
+            }
+        }
+
 
     }
 }
